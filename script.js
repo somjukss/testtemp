0392b9f826dc635fb6fe66828ba042b1b3cf5f67
somjukss <so_somjuk@hotmail.com>
2020-06-30 16:23:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+//clicking on document flips card to download file
+var imgIcon = document.getElementById('img1');
+
+
+window.onclick = function (event) {
+    // need to add file download
+        var link = document.createElement("a");
+        link.download = "test";
+        link.href = "./test_form.pdf";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        delete link;
+}
